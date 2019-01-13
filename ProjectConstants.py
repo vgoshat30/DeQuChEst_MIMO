52b239e4a5c50c9ada1aa06f422457a615060425
Gosha Tsintsadze <vgoshat30@gmail.com>
2019-01-13 13:09:05 +0200

    CHECKPOINT 3.10.1.1
    Problems with too results being bellow optimal bound

--- ProjectConstants.py
+++ ProjectConstants.py
@@ -2,8 +2,8 @@
 #                     Training, Testing and Logging Data                       #
 ################################################################################
 
-DATA_MAT_FILE = ['data_channel_test.mat', ]
-TEST_LOG_MAT_FILE = 'testLog_channel_test.mat'
+DATA_MAT_FILE = ['data_Gamma.mat', ]
+TEST_LOG_MAT_FILE = 'testLog_Gamma5.mat'
 
 ################################################################################
 #                            Models to Activate                                #
@@ -20,7 +20,7 @@ modelsToActivate = [
 ################################################################################
 
 BATCH_SIZE = 8
-EPOCH_RANGE = [1, ]
+EPOCH_RANGE = [30, ]
 LR_RANGE = [0.002, ]
 M_RANGE = range(2, 100)
 C_INCREMENT_RANGE = [[8, 8],]
@@ -28,11 +28,11 @@ C_STEPS_AMOUNT = 1
 FORCE_QUANTIZATION_DIM = False
 
 ARCHITECTURE = [
-    ['linear', 2,
-     'linear', 0.5,
-     'quantization',
-     'linear', 2,
-     'linear', 0.5],
+    # ['linear', 2,
+    #  'linear', 0.5,
+    #  'quantization',
+    #  'linear', 2,
+    #  'linear', 0.5],
 
     # ['linear', 0.5,
     #  'linear', 0.5,
@@ -40,29 +40,80 @@ ARCHITECTURE = [
     #  'linear', 2,
     #  'linear', 0.5],
 
-    ['linear', 2,
-     'linear', 2,
-     'relu',
-     'linear', 0.5,
-     'linear', 0.5,
-     'quantization',
-     'linear', 0.5,
-     'linear', 0.5,
-     'relu',
-     'linear', 0.5,
-     'linear', 0.5],
+    # ['linear', 2,
+    #  'linear', 2,
+    #  'relu',
+    #  'linear', 0.5,
+    #  'linear', 0.5,
+    #  'quantization',
+    #  'linear', 0.5,
+    #  'linear', 0.5,
+    #  'relu',
+    #  'linear', 0.5,
+    #  'linear', 0.5],
 
-    ['linear', 2,
-     'linear', 2,
+    ['linear', 0.8,
      'relu',
-     'linear', 2,
-     'linear', 2,
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
      'quantization',
-     'linear', 0.5,
-     'linear', 0.25,
+     'linear', 2,
      'relu',
-     'linear', 0.25,
-     'linear', 0.25]
+     'linear', 2,
+     'relu',
+     'linear', 2,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.9,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.8,
+     'relu',
+     'linear', 0.7,
+     'relu',
+     'linear', 0.7
+     ]
 ]
 
 '''Instructions for using ARCHITECTURE constant:
